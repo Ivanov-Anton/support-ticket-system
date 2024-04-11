ca2472b43c0526f051dc34d184fda223878f9348
Anton-Ivanov <anton.i@hey.com>
2024-04-11 16:16:35 +0300

    updated annotate config to display the limit of types "integer, bigint"

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,14 +4,14 @@
 #
 # Table name: tickets
 #
-#  id         :bigint           not null, primary key
+#  id         :bigint(8)        not null, primary key
 #  content    :text
 #  email      :string
 #  name       :string
 #  subject    :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  status_id  :integer
+#  status_id  :integer(2)
 #
 class Ticket < ActiveRecord::Base
 end
