8d87ca86ea421e6243a3287013a6ab14ed3246e9
Anton-Ivanov <anton.i@hey.com>
2024-04-11 15:20:36 +0300

    execute annotate and update Ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: tickets
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  email      :string
+#  name       :string
+#  subject    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  status_id  :integer
+#
 class Ticket < ActiveRecord::Base
 end
