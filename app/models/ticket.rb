047fb2b34f5bb4595cfd2ea6a76613c3fb0882d0
Anton-Ivanov <anton.i@hey.com>
2024-04-11 17:27:45 +0300

    add validation for ticket model
    - Refactored the ticket model to move constants into a module for better organization.
    - Renamed some constant variables to be more descriptive.
    - Added a constant for a valid email regex pattern.
    - Updated the validations to use the refactored constants and the email regex.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -14,4 +14,33 @@
 #  status_id  :integer(2)
 #
 class Ticket < ActiveRecord::Base
+  module CONST # :nodoc:
+    NEW_STATUS_ID = 1
+    NEW_PENDING_ID = 2
+    NEW_RESOLVED_ID = 3
+
+    NEW_STATUS_NAME = 'NEW'
+    NEW_PENDING_NAME = 'PENDING'
+    NEW_RESOLVED_NAME = 'RESOLVED'
+
+    STATUS_IDS = [
+      NEW_STATUS_ID,
+      NEW_PENDING_ID,
+      NEW_RESOLVED_ID
+    ].freeze
+
+    STATUSES = {
+      NEW_STATUS_ID => NEW_STATUS_NAME,
+      NEW_PENDING_ID => NEW_PENDING_NAME,
+      NEW_RESOLVED_ID => NEW_RESOLVED_NAME
+    }.freeze
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+    freeze
+  end
+
+  validates :name, :email, :subject, presence: true
+  validates :status_id, inclusion: { in: Ticket::CONST::STATUS_IDS }
+  validates :email, format: { with: CONST::VALID_EMAIL_REGEX }
 end
